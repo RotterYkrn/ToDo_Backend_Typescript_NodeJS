3c78f0c8a3d50d6de4703dab3d051a0535910f90
RotterYkrn <sugaku.ohno@gmail.com>
2025-04-04 22:37:17 +0900

    フォーマッタ設定

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import prettierConfig from 'eslint-config-prettier';
+import tseslint from "typescript-eslint";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  tseslint.configs.recommended,
+  prettierConfig,
+]);
